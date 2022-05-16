a6ee5bad689c5e9c4bfb14c432988948e0e399b4
Finley Williamson V <finley.williamson@gmail.com>
2022-05-16 12:58:24 -0400

    Correct an issue with the prompt of question #2

--- src/app/challenge-data/challenge-repo.constant.ts
+++ src/app/challenge-data/challenge-repo.constant.ts
@@ -15,7 +15,7 @@ export const ChallengeRepo: IChallengeInfo[] = [
     {
         challengeNumber: 2,
         title: "Challenge #2 - Highest Scoring Word",
-        description: "Given an array of words, find the highest scoring word. Each letter of a word scores points according to it's position in the alphabet. If two words score the same amount, return the first word in the array.",
+        description: "Given an array of words, find the highest scoring word. Each letter of a word scores points according to its position in the alphabet (a = 1, b = 2, ...). If two words score the same amount, return the first word in the array.",
         tests: [
             { input: ["dog", "cat", "bird", "fish"], output: "fish" },
             { input: ["Maine", "New Hampshire", "Vermont", "New York"], output: "New Hampshire" },
