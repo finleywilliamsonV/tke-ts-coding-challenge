171f39cfdb8295475d33952e148bd5b67f35287a
Finley Williamson V <finley.williamson@gmail.com>
2022-05-24 15:33:54 -0400

    Final removal of lodash

--- src/app/components/challenges/challenges.functions.ts
+++ src/app/components/challenges/challenges.functions.ts
@@ -1,5 +1,3 @@
-import { isNil } from 'lodash'
-
 export const getChallengeOutput = (functionOutput: any) => {
     if (functionOutput === null || functionOutput === undefined || functionOutput === '') {
         return '???';
